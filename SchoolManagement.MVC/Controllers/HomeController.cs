c663621e1284801e7bfb7dd45ec0a217c75b8d00
Tawhid Hasan <tawhidhasan44@gmail.com>
2022-12-30 17:07:58 +0600

    Manipulated Website Views and Layout

--- SchoolManagement.MVC/Controllers/HomeController.cs
+++ SchoolManagement.MVC/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace SchoolManagement.MVC.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
